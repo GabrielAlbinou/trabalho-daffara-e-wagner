58cd9e5f85dac01cd50fa7be4428061c2b75ecdc
GabrielBiano <125101449+GabrielAlbinou@users.noreply.github.com>
2024-09-18 08:45:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function cadastro_cl(){
-    const no_cl = document.getElementById('name').value;
-    document.getElementById('nome').style.color='red';
-    document.getElementById('nome').innerText = no_cl;
+    const no_cl = document.getElementById('name_cl').value;
+    const em_cl = document.getElementById('email_cl').value;
+    const tel_cl = document.getElementById('phone_cl').value;
+    const tpTel_cl = document.getElementById('phoneType_cl').value;
+    const end_cl = document.getElementById('address_cl').value;
+    const doc_cl = document.getElementById('Doc_cl').value;
+    const tpDoc_cl = document.getElementById('tp_doc_cl').value;
+    const cod_cl = document.getElementById('codigo_cl').value;
+    const est_cl = document.getElementById('estado_cl').value;
+    const city_cl = document.getElementById('cidade_cl').value;
+
+    document.getElementById('2').innerText = "nome do gay é: " + no_cl;
+    document.getElementById('3').innerText = "email do gay é: " + em_cl;
+    document.getElementById('4').innerText = "telefone do gay é: " + tel_cl;
+    document.getElementById('5').innerText = "tipo do gay é: " + tpTel_cl;
+    document.getElementById('6').innerText = "endereço do gay é: " + end_cl;
+    document.getElementById('7').innerText = "documento do gay é: " + doc_cl;
+    document.getElementById('8').innerText = "tipo do documento do gay é: " + tpDoc_cl;
+    document.getElementById('9').innerText = "codigo do gay é: " + cod_cl;
+    document.getElementById('10').innerText = "estado do gay é: " + est_cl;
+    document.getElementById('11').innerText = "cidade do gay é: " + city_cl;
 }
