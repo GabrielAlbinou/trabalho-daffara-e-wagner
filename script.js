397bc1f4a84cfa2510be790499e095398c5039fd
GabrielBiano <125101449+GabrielAlbinou@users.noreply.github.com>
2024-10-01 10:39:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function cadastro_cl() {
-    document.getElementById('name_result').innerText = document.getElementById('name_cl').value;
-    document.getElementById('email_result').innerText = document.getElementById('email_cl').value;
-    document.getElementById('phone_result').innerText = document.getElementById('phone_cl').value;
-    document.getElementById('phoneType_result').innerText = document.getElementById('phoneType_cl').value;
-    document.getElementById('address_result').innerText = document.getElementById('address_cl').value;
-    document.getElementById('Doc_result').innerText = document.getElementById('Doc_cl').value;
-    document.getElementById('tp_doc_result').innerText = document.getElementById('tp_doc_cl').value;
-    document.getElementById('codigo_result').innerText = document.getElementById('codigo_cl').value;
-    document.getElementById('estado_result').innerText = document.getElementById('estado_cl').value;
-    document.getElementById('cidade_result').innerText = document.getElementById('cidade_cl').value;
+    const na_cl = document.getElementById('name_cl').value;
+    const email_cl = document.getElementById('email_cl').value;
+    const phone_cl = document.getElementById('phone_cl').value;
+    const phoneType_cl = document.getElementById('phoneType_cl').value;
+    const address_cl = document.getElementById('address_cl').value;
+    const Doc_cl = document.getElementById('Doc_cl').value;
+    const tp_doc_cl = document.getElementById('tp_doc_cl').value;
+    const codigo_cl = document.getElementById('codigo_cl').value;
+    const estado_cl = document.getElementById('estado_cl').value;
+    const cidade_cl = document.getElementById('cidade_cl').value;
+
 }
 
 function cadastro_prod(){
@@ -20,16 +21,15 @@ function cadastro_prod(){
     const data = document.getElementById('data').value;
     const comp = document.getElementById('preco_compra').value;
     const vend = document.getElementById('preco_').value;
-    const prod = document.getElementById('descricao').value;
-    const fabr = document.getElementById('descricao').value;
-    document.getElementById('ds').innerText = desc;
+    const qnt_prod = document.getElementById('quantidade').value;
+    const cod_fabr = document.getElementById('codigo_fabricante').value;
 }
 
 function cadastro_fornecedor() {
-    document.getElementById('nc').innerText = document.getElementById('nome_completo').value;
-    document.getElementById('cnpj_span').innerText = document.getElementById('cnpj').value;
-    document.getElementById('fab').innerText = document.getElementById('fabricante').value;
-    document.getElementById('end').innerText = document.getElementById('endereco').value;
+    const nc = document.getElementById('nome_completo').value;
+    const cnpj = document.getElementById('cnpj').value;
+    const fabr = document.getElementById('fabricante').value;
+    const end = document.getElementById('endereco').value;
 }
 
 function gerarIdFuncionario() {
@@ -37,22 +37,22 @@ function gerarIdFuncionario() {
 }
 
 function cadastro_funcionario() {
-    document.getElementById('idf').innerText = document.getElementById('id_funcionario').value;
-    document.getElementById('nome_span').innerText = document.getElementById('nome').value;
-    document.getElementById('cpf_span').innerText = document.getElementById('cpf').value;
-    document.getElementById('dn').innerText = document.getElementById('data_nascimento').value;
-    document.getElementById('mun').innerText = document.getElementById('municipio').value;
-    document.getElementById('uf_span').innerText = document.getElementById('uf').value;
-    document.getElementById('end').innerText = document.getElementById('endereco').value;
-    document.getElementById('cep_span').innerText = document.getElementById('cep').value;
-    document.getElementById('sexo_span').innerText = document.getElementById('sexo').value;
-    document.getElementById('peso_span').innerText = document.getElementById('peso').value;
-    document.getElementById('gi').innerText = document.getElementById('grau_instrucao').value;
-    document.getElementById('pp').innerText = document.getElementById('pis_pasep').value;
-    document.getElementById('da').innerText = document.getElementById('data_admissao').value;
-    document.getElementById('ct').innerText = document.getElementById('carteira_trabalho').value;
-    document.getElementById('serie_span').innerText = document.getElementById('serie').value;
-    document.getElementById('setor_span').innerText = document.getElementById('setor').value;
+    const func_id = document.getElementById('id_funcionario').value;
+    const func_nm = document.getElementById('nome').value;
+    const func_cpf = document.getElementById('cpf').value;
+    const func_nasc = document.getElementById('data_nascimento').value;
+    const func_muni = document.getElementById('municipio').value;
+    const func_uf = document.getElementById('uf').value;
+    const func_end = document.getElementById('endereco').value;
+    const func_cep = document.getElementById('cep').value;
+    const func_sex = document.getElementById('sexo').value;
+    const func_pes = document.getElementById('peso').value;
+    const func_grau = document.getElementById('grau_instrucao').value;
+    const func_pis = document.getElementById('pis_pasep').value;
+    const func_data_ad = document.getElementById('data_admissao').value;
+    const func_cart = document.getElementById('carteira_trabalho').value;
+    const func_serie = document.getElementById('serie').value;
+    const func_set = document.getElementById('setor').value;
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
@@ -60,15 +60,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function cadastro_quarto() {
-    document.getElementById('nq').innerText = document.getElementById('numero_quarto').value;
-    document.getElementById('tq').innerText = document.getElementById('tipo_quarto').value;
+    const num_q = document.getElementById('numero_quarto').value;
+    const tp_quarto = document.getElementById('tipo_quarto').value;
 }
 
 function cadastro_servico() {
-    document.getElementById('ns').innerText = document.getElementById('nome_servico').value;
-    document.getElementById('qt').innerText = document.getElementById('quantidade').value;
-    document.getElementById('ts').innerText = document.getElementById('tipo_servico').value;
-    document.getElementById('idq').innerText = document.getElementById('id_quarto').value;
+    const nm_ser = document.getElementById('nome_servico').value;
+    const qnt_ser = document.getElementById('quantidade').value;
+    const tp_ser = document.getElementById('tipo_servico').value;
+    const id_ser = document.getElementById('id_quarto').value;
 }
 
 function gerarIdQuarto() {
@@ -76,12 +76,12 @@ function gerarIdQuarto() {
 }
 
 function cadastro_tipo_quarto() {
-    document.getElementById('idq').innerText = document.getElementById('id_quarto').value;
-    document.getElementById('tq').innerText = document.getElementById('tipo_quarto').value;
-    document.getElementById('car').innerText = document.getElementById('caracteristicas').value;
-    document.getElementById('equip').innerText = document.getElementById('equipamentos').value;
-    document.getElementById('qi').innerText = document.getElementById('quantidade_itens').value;
-    document.getElementById('desc').innerText = document.getElementById('descricao').value;
+    const id_quarto = document.getElementById('id_quarto').value;
+    const tp_qto = document.getElementById('tipo_quarto').value;
+    const cara_quarto = document.getElementById('caracteristicas').value;
+    const equi_quarto = document.getElementById('equipamentos').value;
+    const qnt_itens = document.getElementById('quantidade_itens').value;
+    const desc_quarto = document.getElementById('descricao').value;
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
